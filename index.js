3d72602d12354c014689e827d2ecdc1d9535aded
Nick Corona <coronaisme@gmail.com>
2019-11-19 20:05:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ function takeANumber(katzDeliLine, customer) {
   katzDeliLine.push(customer);
     return `Welcome, ${customer}. You are number ${(katzDeliLine.indexOf(customer)) + 1} in line.`;
 }
+function takeANumber2(katzDeliLine){
+  //add customer to line then welcomes customer
+  //i: numbers
+  //o: numbers with a String
+  count = 0;
+  count++;
+  //increment the count
+  katzDeliLine.push(count);
+  //add to array
+  return `Welcome, you are number ${count} in line`
+  
+}
 
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length < 1) {
